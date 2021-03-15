89f7086aeab207e13c2ea3134c00d4576001c4fc
talhaarshad96 <arshad.talha18@gmail.com>
2021-03-16 00:38:13 +0500

    mac 2

--- src/com/nisum/training/myOwnStuff.java
+++ src/com/nisum/training/myOwnStuff.java
@@ -1,2 +1,25 @@
-package com.nisum.training;public class myOwnStuff {
+package com.nisum.training;
+
+class myOwnStuff
+{
+        public static void main(String[] args)
+        {
+            String s = new String("RR");
+            s = null;
+
+            //now if I want to clear the Class garbage collector I need to call Class object.
+            myOwnStuff s1 = new myOwnStuff();
+            s1=null;
+
+
+            // Requesting JVM to call Garbage Collector method
+            System.gc();
+            System.out.println("Main Completes");
+        }
+
+        // Here overriding finalize method
+        public void finalize()
+        {
+            System.out.println("finalize method overriden");
+        }
 }
